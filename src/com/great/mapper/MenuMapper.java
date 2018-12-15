cd0777be8f9a7466e1de2ad0c0bf322229fa418f
ys19961230 <2801937807@qq.com>
2018-12-16 02:44:16 +0800

    123mapper

--- src/com/great/mapper/MenuMapper.java
+++ src/com/great/mapper/MenuMapper.java
@@ -6,7 +6,10 @@ import java.util.Map;
 import com.great.bean.Menu;
 
 public interface MenuMapper {
-	public List<Map<String,Object>> queryAllMenu();//查询所有菜单
+	public List<Map<String,Object>> queryAllMenu();//查询所有菜单111111
 	public List<Map<String,Object>> queryFirstMenu();//查询一级菜单
 	public Integer createNewMenu(Menu menu);//新增菜单
+	public Integer manageMenu(Integer menuId);//管理菜单(启用)
+	public Integer stopMenu(Integer menuId);//禁用菜单
+	public List<Map<String,Object>> queryLeftMenu(int roleId);//查询主页左侧菜单（只查询可用菜单）
 }
