00daa62f0c4a9cd20397635f9e17419203888d70
ys19961230 <2801937807@qq.com>
2018-12-16 00:41:10 +0800

    1216

--- src/com/great/handler/AdminHandler.java
+++ src/com/great/handler/AdminHandler.java
@@ -46,7 +46,7 @@ public class AdminHandler {
 		//if (keyCode.toLowerCase().equals(code.toLowerCase())) {
 			if (temp != null) {
 				session.setAttribute("loggingAdmin", temp);
-				mav.setViewName("forward:/backstage/backmain.jsp");
+				mav.setViewName("forward:/menuHandler/getLeftMenu.action");
 			} else {
 				mav.setViewName("forward:/backstage/backlogin.jsp");
 			}
